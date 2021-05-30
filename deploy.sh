f783e354b816bc07248540d6e47cb55a53e036db
Xiang YiSheng <1697504283@qq.com>
2021-05-31 00:03:45 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,7 @@
 set -e
 
 # 生成静态文件
-npm install
-npm run build
+npm run docs:build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -14,11 +13,11 @@ cd docs/.vuepress/dist
 # echo 'www.example.com' > CNAME
 
 git init
-git checkout -b gh-pages && git add .
+git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:xiangyisheng/xiangyisheng.github.io.git main
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:xiangyisheng/vuepress.git main:gh-pages
