e018ac3128657b5123ad637e40ce39d7459493f3
Xiang YiSheng <1697504283@qq.com>
2021-05-30 23:37:18 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,22 +4,16 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+npm run build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
-
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:xiangyisheng/xiangyisheng.github.io.git main
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:xiangyisheng/vuepress.git main:gh-pages
+git push -f git@github.com:dwanda/dwanda.github.io.git master
 
 cd -
